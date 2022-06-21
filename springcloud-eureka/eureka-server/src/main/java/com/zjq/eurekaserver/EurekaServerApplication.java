5f825a90e2f0e2eed50a67cdf8962781ca68ebf0
zhanjq <1187625098@qq.com>
2022-06-21 15:54:19 +0800

    eureka-server

---
+++ springcloud-eureka/eureka-server/src/main/java/com/zjq/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.zjq.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * Eureka 服务端
+ * @EnableEurekaServer 表示是Eureka 服务端
+ * @author zjq
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
